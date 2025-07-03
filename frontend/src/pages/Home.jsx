2f98fe212a5053bbfb419645650287ed0ec162d7
sandipv2 <sandiipvarma@gmail.com>
2025-07-03 14:32:39 +0530

    modified/homePage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
     <div className='min-h-screen bg-linear-120 from-indigo-200 to-indigo-400 flex flex-col items-center justify-center gap-2'>
       <h3 className='text-xl'>Hey {userData ? userData.name : 'Developers'}! 👋</h3>
       <h1 className='text-4xl font-extrabold'>Welcome To Our App</h1>
-      <p className='text-lg'>We beleive in quality and customer satifaction.</p>
+      <p className='text-lg'>We believe in quality and customer satisfaction.</p>
     </div>
   )
 }
